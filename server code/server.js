15f5472a8a7c1a7bd54aaed8701b89ac457bae86
Justin Kelley <jckelley15@comcast.net>
2017-10-14 11:22:27 -0400

    yo

---
+++ server code/server.js
@@ -0,0 +1,188 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8081;
+const path = require('path');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const request = require('request');
+
+// For loading API keys with dotenv
+require('dotenv').config()
+
+app.use(cookieParser());
+app.use(bodyParser());
+
+// Authenticating to Yelp API
+request.post(
+    'https://api.yelp.com/oauth2/token',
+    { json: 
+    	{ 
+    		client_id: process.env.YELP_API_KEY,
+    		client_secret: process.env.YELP_SECRET_KEY
+    	} 
+	},
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
+
+app.listen(port, function() {
+	console.log('Express server listening on port 8081');
+});
+
+/** Request should be a JSON object in the form of:
+Activities JSON {'Activities': [{
+				'Name': String
+				'startTime': String
+				'endTime': String
+				'location': String
+			}]
+		'PreferredTimes': {
+				'Breakfast': {
+					'StartTime': String
+					'EndTime': String
+				}
+				'Lunch': {
+					'StartTime': String
+					'EndTime': String
+				}
+				'Dinner': {
+					'StartTime': String
+					'EndTime': String
+				}
+	}
+} */
+app.post('/mealTimes', function(req, res) {
+	//req.body.schedule is the Activities JSON passed in the post request
+	mealTimeJSON = findBestMealTimes(req.body.schedule);
+	res.json(mealTimeJSON);
+});
+
+/**
+	Takes in an Activities JSON object
+
+	Return a mealTime JSON object in the form of:
+	mealTime JSON {
+		'Breakfast': {
+				'StartTime': String
+				'EndTime': String
+			}
+		'Lunch': {
+			'StartTime': String
+			'EndTime': String
+		}
+		'Dinner': {
+			'StartTime': String
+			'EndTime': String
+	}
+
+} */
+function findBestMealTimes(mealTimeJSON) {
+	const range = 30;
+	var bStart = getTime(mealTimeJSON.PreferredTimes.Breakfast.StartTime);
+	bStart = addMintoTime(bStart, -range);
+	var bEnd = getTime(mealTimeJSON.PreferredTimes.Breakfast.EndTime);
+	bEnd = addMintoTime(bEnd, range);
+
+	var lStart = getTime(mealTimeJSON.PreferredTimes.Lunch.StartTime);
+	lStart = addMintoTime(lStart, -range);
+	var lEnd = getTime(mealTimeJSON.PreferredTimes.Lunch.EndTime);
+	lEnd = addMintoTime(lEnd, range);
+
+	var dStart = getTime(mealTimeJSON.PreferredTimes.Dinner.StartTime);
+	dStart = addMintoTime(dStart, -range);
+	var dEnd = getTime(mealTimeJSON.PreferredTimes.Dinner.EndTime);
+	dEnd = addMintoTime(dEnd, range);
+
+	var resBStart = null, resBEnd = null, resLStart = null, resLEnd = null,
+			resDStart = null, resDEnd = null;
+
+	// find max time interval within range for preferred breakfast time
+	activities = mealTimeJSON.Activities;
+	var currEndTime, currStartTime;
+	for (var i = 0; i + 1 < activities.length; i++) {
+			currEndTime = getTime(activities[i].endTime);
+		  currStartTime = getTime(activities[i + 1].StartTime);
+			if (compareTimes(currEndTime, bStart) >= 0 && compareTimes(currStartTime, bEnd) <= 0) {
+				if (resBStart == null && resBEnd == null) {
+					resBStart = currEndTime;
+					resBEnd = currStartTime;
+				} else if (timeDifference(currEndTime, currStartTime) > timeDifference(resBEnd, resBStart)) {
+						resBStart = currEndTime;
+						resBEnd = currStartTime;
+				}
+			} else if (compareTimes(currEndTime, lStart) >= 0 && compareTimes(currStartTime, lEnd) <= 0) {
+				if (resLStart == null && resLEnd == null) {
+					resLStart = currEndTime;
+					resLEnd = currStartTime;
+				} else if (timeDifference(currEndTime, currStartTime) > timeDifference(resLEnd, resLStart)) {
+					resLStart = currEndTime;
+					resLEnd = currStartTime;
+				}
+
+			} else if (compareTimes(currEndTime, dStart) >= 0 && compareTimes(currStartTime, dEnd) <= 0) {
+					if (resDStart == null && resDEnd == null) {
+						resDStart = currEndTime;
+						resDEnd = currStartTime;
+					} else if (timeDifference(currEndTime, currStartTime) > timeDifference(resDEnd, resDStart)) {
+						resDStart = currEndTime;
+						resDEnd = currStartTime;
+					}
+			}
+	}
+
+	var resJSON = {
+		'Breakfast': {
+			'StartTime': resBStart,
+			'EndTime': resBEnd,
+		},
+		'Lunch': {
+			'StartTime': resLStart,
+			'EndTime': resLEnd
+		},
+		'Dinner': {
+			'StartTime': resDStart,
+			'EndTime': resDEnd
+		}
+	}
+	return resJSON;
+}
+
+function getTime(time) {
+    var timeString = JSON.parse(time); //example JSON string: "23:00"
+    var arr = timeString.split(":");
+    var hours = parseInt(arr[0]) * 60;
+    return hours + arr[1];
+}
+
+function minToString(min) {
+    if (min == null) {
+        return "No time available.";
+    } else {
+        var hours = min/60;
+        var min = min%60;
+    }
+    return hours + ":" + min;
+}
+
+function addMintoTime(oldTime, min) {
+	return oldTime + min;
+}
+
+function timeDifference(date1, date2) {
+    return date2 - date1;
+}
+
+function compareTimes(date1, date2) {
+	if (timeDifference(date1, date2) < 0) {
+		return -1;
+	} else if (timeDifference(date1, date2) == 0) {
+		return 0;
+	return 1;
+
+}
