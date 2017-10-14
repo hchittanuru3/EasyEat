69fa8992a16e7534a17e42d57a60ae4da4dbdad4
hchittanuru3 <hemanthc98@outlook.com>
2017-10-14 01:10:21 -0400

    yo

--- server code/server.js
+++ server code/server.js
@@ -64,6 +64,14 @@ function findBestMealTimes(mealTimeJSON) {
 
 }
 
-function stringToTimeDecimal(time) {
+function getTime(time) {
+    var timeString = JSON.parse(time); //example JSON string: "2014-01-01T23:28:56.782Z"
+    var time = new Date(timeString);
+    return time;
+}
 
+function timeDifference(date1, date2) {
+    var diff = date2 - date1;
+    var min = diff/60;
+    return min;
 }
