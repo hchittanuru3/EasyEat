6f17bad584ad725d0f09ef4428ebcd5a4f8311db
hchittanuru3 <hemanthc98@outlook.com>
2017-10-14 18:32:40 -0400

    started interfacing

--- app/src/main/java/com/example/hemanthc98/hackgt/loginScreen.java
+++ app/src/main/java/com/example/hemanthc98/hackgt/loginScreen.java
@@ -1,5 +1,6 @@
 package com.example.hemanthc98.hackgt;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
@@ -11,22 +12,34 @@ import android.widget.EditText;
 
 public class loginScreen extends AppCompatActivity {
 
-    EditText username = (EditText) findViewById(R.id.username);
-    EditText password = (EditText) findViewById(R.id.password);
-    Button login = (Button) findViewById(R.id.login);
+    EditText username;
+    EditText password;
+    Button login;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String user = username.getText().toString();
                 String pass = MainActivity.makePassword(password.getText().toString());
-                Intent intent = new Intent(loginScreen.this, activitiesScreen.class);
-                startActivity(intent);
+                if (APIInterfacer.authenticateUser(user, pass)) {
+                    Intent intent = new Intent(loginScreen.this, activitiesScreen.class);
+                    startActivity(intent);
+                } else {
+                    String title = "Error";
+                    String message = "Login credentials are incorrect.";
+                    new AlertDialog.Builder(loginScreen.this)
+                            .setMessage(message)
+                            .setTitle(title)
+                            .setPositiveButton("OK", null);
+                }
             }
         });
     }
