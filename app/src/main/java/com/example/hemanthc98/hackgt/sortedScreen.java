d5d86fd9545d858981777e33e5ede4ff2b5c0651
hchittanuru3 <hemanthc98@outlook.com>
2017-10-15 01:13:51 -0400

    idk

--- app/src/main/java/com/example/hemanthc98/hackgt/sortedScreen.java
+++ app/src/main/java/com/example/hemanthc98/hackgt/sortedScreen.java
@@ -2,8 +2,14 @@ package com.example.hemanthc98.hackgt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class sortedScreen extends AppCompatActivity {
 
@@ -15,5 +21,14 @@ public class sortedScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorted_screen);
         spinner.setPrompt("Sort By:");
+        spinner.setAdapter(getCriteria());
+
+    }
+
+    private ArrayAdapter<String> getCriteria() {
+        List<String> list = new ArrayList<>(Arrays.asList("Rating", "Est. Arrival", "Price"));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
     }
 }
